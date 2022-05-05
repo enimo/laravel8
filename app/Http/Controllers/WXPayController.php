55549e623e4d002ad032e03731c6877288cac88d
enimo <enimong@gmail.com>
2022-05-06 00:55:05 +0800

    solve unselect problem

--- app/Http/Controllers/WXPayController.php
+++ app/Http/Controllers/WXPayController.php
@@ -89,7 +89,7 @@ class WXPayController extends Controller
             $resp = WXPayController::$instance2->request('POST', 'https://api.mch.weixin.qq.com/v3/pay/transactions/native', [
                 'json' => [
                     'mchid' => env('MCHID'),
-                    'out_trade_no' => '666554001',
+                    'out_trade_no' => '666554001'.date("Ymd"),
                     'appid' => env('MP_APPID'),
                     'description' => 'Image形象店-深圳腾大-QQ公仔',
                     'notify_url' => 'https://weixin.qq.com/',
